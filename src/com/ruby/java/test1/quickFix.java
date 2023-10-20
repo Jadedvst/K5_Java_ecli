284e81c88f76f3c6516cc7ed5b8ba6c9b3ed7bf3
pnusw14 <dustofjade@gmail.com>
2023-10-20 13:31:02 +0900

    javav1000

---
+++ src/com/ruby/java/test1/quickFix.java
@@ -0,0 +1,11 @@
+package com.ruby.java.test1;
+//quickFix로 지정된곳으로 바로 이동 가능
+// 현 폴더에서 com 안의 ruby 안의 ... 에 파일 생성
+public class quickFix {
+
+//	public static void main(String[] args) {
+//		System.out.println("Hi");
+
+//	}
+
+}
